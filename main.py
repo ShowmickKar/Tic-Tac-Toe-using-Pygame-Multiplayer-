928f025b60bd01b1bcf45a3b27076bc5b4bbba84
Showmick Kar <showmick12@gmail.com>
2020-10-05 01:52:13 +0600

    Update and rename src.py to main.py
    Added Documentation

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# This is a multiplayer Tic Tac Toe game I made using the module Pygame
+# To run the source coude, you need to have python3 and pygame installed on your computer
+# If you are on windows, you can simply download this executable file(link is given below) to run it directly on you PC
+# Executable file link: https://github.com/ShowmickKar/Tic-Tac-Toe-using-Pygame-Multiplayer-/blob/main/TIC-TAC_Toe_Executable.rar
+# I hope you enjoy!
+
 import pygame
 pygame.init()
 
@@ -12,6 +18,8 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 INDIGO = (70, 130, 255)
 
+
+# Initialized attributes of nine squares of the board including their positions and colors
 first = pygame.draw.rect(screen, WHITE, (20, 20, 170, 170))
 second = pygame.draw.rect(screen, WHITE, (210, 20, 170, 170))
 third = pygame.draw.rect(screen, WHITE, (400, 20, 170, 170))
@@ -22,14 +30,20 @@ seventh = pygame.draw.rect(screen, WHITE, (20, 400, 170, 170))
 eight = pygame.draw.rect(screen, WHITE, (210, 400, 170, 170))
 ninth = pygame.draw.rect(screen, WHITE, (400, 400, 170, 170))
 
+
+# Initialize two lists to keep track of the 9 sqares in the game
 board_position_name = []
 board_position = []
+
+# This list keeps track of all the nine squares and their atributes
 board_position_name.extend([0, first, second, third, fourth, fifth, sixth, seventh, eight, ninth])
+
+# This square keeps track of the positions of all of the nine sqaures
 board_position.extend([0, [20, 20], [210, 20], [400, 20], [20, 210], [210, 210], [400, 210], [20, 400], [210, 400], [400, 400]])
 
 grid = [0, "", "", "", "", "", "", "", "", ""]
 
-
+# Determine if someone has won and if so return the winner
 def check_win():
 	if grid[1] == "X" and grid[5] == "X" and grid[9] == "X":
 		return "First Player"
@@ -67,10 +81,17 @@ def check_win():
 	else:
 		return "No one"
 
-
+# Determine if it's a draw
 def check_draw():
+	# If the remaining moves is zero then two things can happen
+	# 1. Either Player 1 or Player 2 has won
+	# 2. It's a draw
+	# We take care of case when it's not a draw before calling this function
+	# That is, if this function return True, it's definitely a draw
 	return not remaining_moves
 
+
+# Initialized a varible that changes the player after each move
 toggle: bool = True
 
 remaining_moves = 9
