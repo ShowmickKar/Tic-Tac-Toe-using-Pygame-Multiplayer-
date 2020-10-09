58829a5ff6edbf5941a8d4da363c79b74eb0e244
Showmick Kar <showmick12@gmail.com>
2020-10-09 12:53:42 +0600

    Update main.py
    * Added a "PLAY AGAIN" option, so the game window does not close after each game.
    * Added real crosses and zeroes instead of rectangle and circles.

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 # This is a multiplayer Tic Tac Toe game I made using the module Pygame
 # To run the source coude, you need to have python3 and pygame installed on your computer
 # If you are on windows, you can simply download this executable file(link is given below) to run it directly on you PC
-# Executable file link: https://github.com/ShowmickKar/Tic-Tac-Toe-using-Pygame-Multiplayer-/blob/main/Tic-Tac-Toe%20Multiplayer(executable).rar
+# Executable file link: https://github.com/ShowmickKar/Tic-Tac-Toe-using-Pygame-Multiplayer-/blob/main/TIC-TAC_Toe_Executable.rar
 # I hope you enjoy!
 
 import pygame
 import sys
+
 pygame.init()
 
 (width, length) = (590, 590)
 background_color = (0, 0, 0)
+
+# Initialized a varible that changes the player after each move
+toggle: bool = True
 
 screen = pygame.display.set_mode((width, length))
 screen.fill(background_color)
@@ -18,6 +22,7 @@ pygame.display.set_caption("TIC TAC TOE using Pygame(Multiplayer)")
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 INDIGO = (70, 130, 255)
+GREEN = (0, 255, 0)
 
 
 # Initialized attributes of nine squares of the board including their positions and colors
@@ -45,6 +50,7 @@ board_position.extend([0, [20, 20], [210, 20], [400, 20], [20, 210], [210, 210],
 grid = [0, "", "", "", "", "", "", "", "", ""]
 
 # Determine if someone has won and if so return the winner
+
 def check_win():
 	if grid[1] == "X" and grid[5] == "X" and grid[9] == "X":
 		return "First Player"
@@ -92,19 +98,29 @@ def check_draw():
 	return not remaining_moves
 
 
-# Initialized a varible that changes the player after each move
-toggle: bool = True
+# Setting up a rematch
+def setup():
+	grid = [None, "", "", "", "", "", "", "", "", ""]
+	board_position_name = []
+	board_position = []
+	board_position_name.extend([None, first, second, third, fourth, fifth, sixth, seventh, eight, ninth])
+	board_position.extend([None, [20, 20], [210, 20], [400, 20], [20, 210], [210, 210], [400, 210], [20, 400], [210, 400], [400, 400]])
+	return (grid, board_position_name, board_position)
 
 remaining_moves = 9
+
+flag = False
 
 while True:
 	pygame.time.delay(100)
 	for event in pygame.event.get():
+		if flag and event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_SPACE:
+				flag = False
 		if event.type == pygame.QUIT:
 			pygame.quit()
-			# quit()
-			# raise SystemExit
-			sys.exit()
+			quit()
+			raise SystemExit
 
 		if pygame.MOUSEBUTTONUP == event.type:
 			click_position = pygame.mouse.get_pos()
@@ -118,14 +134,16 @@ while True:
 						grid[position] = "O"
 
 					if toggle:
-						board_position_name[position] = pygame.draw.rect(screen, (0, 255, 0), (
-							board_position[position][0] + 30, board_position[position][1] + 30, 110, 110))
+						img = pygame.image.load("cross_icon.png")
+						img = pygame.transform.scale(img, (120, 120))
+						screen.blit(img, (board_position[position][0] + 27, board_position[position][1] + 27))
+						pygame.display.update()
 						toggle = False
 					else:
-						board_position_name[position] = pygame.draw.circle(screen, (255, 0, 0), (
-							board_position[position][0] + 85, board_position[position][1] + 85), int(110 / 2))
-						# might use the following line instead
-						# board_position_name[position] = pygame.draw.rect(screen, (0, 255, 255), (board_position[position][0] + 30, board_position[position][1] + 30, 110, 110))
+						img = pygame.image.load("circle_icon.png")
+						img = pygame.transform.scale(img, (120, 120))
+						screen.blit(img, (board_position[position][0] + 27, board_position[position][1] + 27))
+						pygame.display.update()
 						toggle = True
 
 					remaining_moves -= 1
@@ -137,46 +155,81 @@ while True:
 			screen.fill((0, 0, 0))
 			pygame.display.update()
 			text_surface= pygame.image.load("first_player.win.png")
-			screen.blit(text_surface, (0, 0))
+			screen.blit(text_surface, (20, 0))
 			pygame.display.update()
 			pygame.time.delay(1000)
-			pygame.quit()
-			# quit()
-			# raise SystemExit
-			sys.exit()
+			print(grid)
+			toggle  = not toggle
+			(grid, board_position_name, board_position) = setup()
+			screen.fill(background_color)
+			remaining_moves = 9
+			first = pygame.draw.rect(screen, WHITE, (20, 20, 170, 170))
+			second = pygame.draw.rect(screen, WHITE, (210, 20, 170, 170))
+			third = pygame.draw.rect(screen, WHITE, (400, 20, 170, 170))
+			fourth = pygame.draw.rect(screen, WHITE, (20, 210, 170, 170))
+			fifth = pygame.draw.rect(screen, WHITE, (210, 210, 170, 170))
+			sixth = pygame.draw.rect(screen, WHITE, (400, 210, 170, 170))
+			seventh = pygame.draw.rect(screen, WHITE, (20, 400, 170, 170))
+			eight = pygame.draw.rect(screen, WHITE, (210, 400, 170, 170))
+			ninth = pygame.draw.rect(screen, WHITE, (400, 400, 170, 170))
+			pygame.display.update()
+			continue
 
 		if check_win() == "Second Player":
 			pygame.time.delay(500)
 			screen.fill((0, 0, 0))
 			pygame.display.update()
 			text_surface = pygame.image.load("second_player_win.png")
-			screen.blit(text_surface, (0, 0))
+			screen.blit(text_surface, (20, 0))
 			pygame.display.update()
 			pygame.time.delay(1000)
-			pygame.quit()
-			# quit()
-			# raise SystemExit
-			sys.exit()
+			print(grid)
+			toggle  = not toggle
+			(grid, board_position_name, board_position) = setup()
+			screen.fill(background_color)
+			remaining_moves = 9
+			first = pygame.draw.rect(screen, WHITE, (20, 20, 170, 170))
+			second = pygame.draw.rect(screen, WHITE, (210, 20, 170, 170))
+			third = pygame.draw.rect(screen, WHITE, (400, 20, 170, 170))
+			fourth = pygame.draw.rect(screen, WHITE, (20, 210, 170, 170))
+			fifth = pygame.draw.rect(screen, WHITE, (210, 210, 170, 170))
+			sixth = pygame.draw.rect(screen, WHITE, (400, 210, 170, 170))
+			seventh = pygame.draw.rect(screen, WHITE, (20, 400, 170, 170))
+			eight = pygame.draw.rect(screen, WHITE, (210, 400, 170, 170))
+			ninth = pygame.draw.rect(screen, WHITE, (400, 400, 170, 170))
+			pygame.display.update()
+			continue
 
 		if check_draw():
 			pygame.time.delay(500)
 			screen.fill((0, 0, 0))
 			pygame.display.update()
 			text_surface = pygame.image.load("draw.png")
-			screen.blit(text_surface, (0, 0))
+			screen.blit(text_surface, (20, 0))
 			pygame.display.update()
 			pygame.time.delay(1000)
-			pygame.quit()
-			# quit()
-			# raise SystemExit
-			sys.exit()
+			print(grid)
+			toggle  = not toggle
+			(grid, board_position_name, board_position) = setup()
+			screen.fill(background_color)
+			remaining_moves = 9
+			first = pygame.draw.rect(screen, WHITE, (20, 20, 170, 170))
+			second = pygame.draw.rect(screen, WHITE, (210, 20, 170, 170))
+			third = pygame.draw.rect(screen, WHITE, (400, 20, 170, 170))
+			fourth = pygame.draw.rect(screen, WHITE, (20, 210, 170, 170))
+			fifth = pygame.draw.rect(screen, WHITE, (210, 210, 170, 170))
+			sixth = pygame.draw.rect(screen, WHITE, (400, 210, 170, 170))
+			seventh = pygame.draw.rect(screen, WHITE, (20, 400, 170, 170))
+			eight = pygame.draw.rect(screen, WHITE, (210, 400, 170, 170))
+			ninth = pygame.draw.rect(screen, WHITE, (400, 400, 170, 170))
+			pygame.display.update()
+			continue
 
 		if event.type == pygame.QUIT:
 			run = False
 			pygame.quit()
-			# quit()
-			# raise SystemExit
-			sys.exit()
+			quit()
+			raise SystemExit
 
 	pygame.display.flip()
 
